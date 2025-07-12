74df54eea5091864b0e65bafe7bc8e057ffe19f1
Chad <chawagon03@gmail.com>
2025-07-11 21:09:39 -0400

    refactor(core/services/install.sh): add .env file creation and update docker compose command to use environment files

--- core/services/install.sh
+++ core/services/install.sh
@@ -22,34 +22,35 @@ mkdir -p $service_path/core/mongo/{config,data}
 mkdir -p $service_path/core/pocket-id/data
 mkdir -p $service_path/core/tsbridge/data
 
+### CREATE .ENV
+cat > $service_path/core/.env <<EOF
+SERVICE_PATH: $service_path
+EOF
+
 ### CREATE .ENV.KOMODO
 cat > $service_path/core/.env.komodo <<EOF
 SERVICE: komodo
-SERVICE_PATH: $service_path
 EOF
 
 ### CREATE .ENV.MONGO
 cat > $service_path/core/.env.mongo <<EOF
 SERVICE: mongo
-SERVICE_PATH: $service_path
 EOF
 
 ### CREATE .ENV.POCKET-ID
 cat > $service_path/core/.env.pocket-id <<EOF
 SERVICE: pocket-id
-SERVICE_PATH: $service_path
 EOF
 
 ### CREATE .ENV.TSBRIDGE
 cat > $service_path/core/.env.tsbridge <<EOF
 SERVICE: tsbridge
-SERVICE_PATH: $service_path
 TS_OAUTH_CLIENT_ID: $tailscale_oauth_id
 TS_OAUTH_CLIENT_SECRET: $tailscale_oauth_secret
 EOF
 
 ### GET COMPOSE FILE
-curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services//compose.yaml
+curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services/compose.yaml
 
 ### START SERVICE(S)
-docker compose -f $service_path/core/compose.yaml up -d
+docker compose -f $service_path/core/compose.yaml --env-file .env --env-file .env.komodo --env-file .env.mongo --env-file .env.pocket-id --env-file .env.tsbridge up -d #> /dev/null 2>&1
