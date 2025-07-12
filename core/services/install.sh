68131b99075bb64bc410bcd0cb1a266eda748183
Chad <chawagon03@gmail.com>
2025-07-11 22:09:11 -0400

    refactor(install.sh): update env file paths to be relative to service directory

--- core/services/install.sh
+++ core/services/install.sh
@@ -53,4 +53,4 @@ EOF
 curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services/compose.yaml
 
 ### START SERVICE(S)
-docker compose -f $service_path/core/compose.yaml --env-file .env --env-file .env.komodo --env-file .env.mongo --env-file .env.pocket-id --env-file .env.tsbridge up -d #> /dev/null 2>&1
+docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.pocket-id --env-file $service_path/core/.env.tsbridge up -d #> /dev/null 2>&1
