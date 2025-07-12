a09eafab1ec7ca585fa76467f1e6b9df93553f20
Chad <chawagon03@gmail.com>
2025-07-11 22:37:10 -0400

    fix(core/services/uninstall.sh, core/services/update.sh): add environment files to docker compose commands for comprehensive configuration management

--- core/services/uninstall.sh
+++ core/services/uninstall.sh
@@ -5,7 +5,7 @@ read -p '[REQUIRED] SERVICE PATH [/opt/services]: ' service_path < /dev/tty
 service_path=${service_path:-'/opt/services'}
 
 ### STOP SERVICE(S)
-docker compose -f $service_path/core/compose.yaml down --rmi all --volumes
+docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.pocket-id --env-file $service_path/core/.env.tsbridge down --rmi all --volumes
 
 ### REMOVE SERVICE FILES/DIRECTORIES
 doas rm -rf $service_path/core
--- core/services/update.sh
+++ core/services/update.sh
@@ -8,4 +8,4 @@ service_path=${service_path:-'/opt/services'}
 curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services//compose.yaml
 
 ### START SERVICE(S)
-docker compose -f $service_path/core/compose.yaml up -d
+docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.pocket-id --env-file $service_path/core/.env.tsbridge up -d
