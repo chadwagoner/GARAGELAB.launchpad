30403312a7f634157342c7cf6d29b1fddbcdce35
Chad <chawagon03@gmail.com>
2025-07-12 00:02:43 -0400

    fix(update.sh): correct typo in compose.yaml URL to ensure proper file retrieval

--- core/services/update.sh
+++ core/services/update.sh
@@ -5,7 +5,7 @@ read -p '[REQUIRED] SERVICE PATH [/opt/services]: ' service_path < /dev/tty
 service_path=${service_path:-'/opt/services'}
 
 ### GET COMPOSE FILE
-curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services//compose.yaml
+curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services/compose.yaml
 
 ### START SERVICE(S)
 docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.pocket-id --env-file $service_path/core/.env.tsbridge up -d
