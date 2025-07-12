0b4d588ef9805f9fdcd8a81ecbd36c9deb5a70df
Chad <chawagon03@gmail.com>
2025-07-12 00:03:01 -0400

    feat(core/agents): add scripts for installing, configuring, updating, and uninstalling the komodo-agent service

---
+++ core/agents/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### USER INPUT VARIABLES
+read -p '[REQUIRED] SERVICE PATH [/opt/services]: ' service_path < /dev/tty
+service_path=${service_path:-'/opt/services'}
+
+### MAKE CORE AGENTS DIRECTORIES
+mkdir -p $service_path/agents/komodo-agent
+
+### CREATE .ENV
+cat > $service_path/agents/.env <<EOF
+SERVICE_PATH: $service_path
+EOF
+
+### CREATE .ENV.KOMODO-AGENT
+cat > $service_path/agents/.env.komodo-agent <<EOF
+SERVICE: komodo-agent
+EOF
+
+### GET COMPOSE FILE
+curl -sL -o $service_path/agents/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/agents/compose.yaml
+
+### START SERVICE(S)
+docker compose -f $service_path/agents/compose.yaml --env-file $service_path/agents/.env --env-file $service_path/agents/.env.komodo-agent up -d > /dev/null 2>&1
