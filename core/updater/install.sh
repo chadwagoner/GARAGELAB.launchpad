b970d8f7b59fe90880c780e3ab03e1fe8721881a
Chad <chawagon03@gmail.com>
2025-07-20 17:21:03 -0400

    fix(install.sh): escape dollar signs in docker container list filters to avoid syntax errors

--- core/updater/install.sh
+++ core/updater/install.sh
@@ -16,7 +16,7 @@ if [[ -d $service_path/core ]]; then
   if [[ \$local_services != \$remote_services ]]; then
     curl -sL -o $service_path/core/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/services/compose.yaml
 
-    if [[ -n "\$(docker container list --filter 'name=^tsbridge$' --quiet)"]]; then
+    if [[ -n '\$(docker container list --filter 'name=^tsbridge$' --quiet)' ]]; then
       docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.id --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.tsbridge up tsbridge -d > /dev/null 2>&1
 
       sleep 30
@@ -24,7 +24,7 @@ if [[ -d $service_path/core ]]; then
       echo "WARNING: TSBRIDGE DOES NOT EXIST... Skipping"
     fi
 
-    if [[ -n "\$(docker container list --filter 'name=^id$' --quiet)"]]; then
+    if [[ -n '\$(docker container list --filter 'name=^id$' --quiet)' ]]; then
       docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.id --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.tsbridge up id -d > /dev/null 2>&1
 
       sleep 30
@@ -32,7 +32,7 @@ if [[ -d $service_path/core ]]; then
       echo "WARNING: ID DOES NOT EXIST... Skipping"
     fi
 
-    if [[ -n "\$(docker container list --filter 'name=^mongo$' --quiet)"]]; then
+    if [[ -n '\$(docker container list --filter 'name=^mongo$' --quiet)' ]]; then
       docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.id --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.tsbridge up mongo -d > /dev/null 2>&1
 
       sleep 30
@@ -40,7 +40,7 @@ if [[ -d $service_path/core ]]; then
       echo "WARNING: MONGO DOES NOT EXIST... Skipping"
     fi
 
-    if [[ -n "\$(docker container list --filter 'name=^komodo$' --quiet)"]]; then
+    if [[ -n '\$(docker container list --filter 'name=^komodo$' --quiet)' ]]; then
       docker compose -f $service_path/core/compose.yaml --env-file $service_path/core/.env --env-file $service_path/core/.env.id --env-file $service_path/core/.env.komodo --env-file $service_path/core/.env.mongo --env-file $service_path/core/.env.tsbridge up komodo -d > /dev/null 2>&1
 
       sleep 30
@@ -58,7 +58,7 @@ if [[ -d $service_path/agents ]]; then
   if [[ \$local_agents != \$remote_agents ]]; then
     curl -sL -o $service_path/agents/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/agents/compose.yaml
 
-    if [[ -n "\$(docker container list --filter 'name=^komodo-agent$' --quiet)"]]; then
+    if [[ -n '\$(docker container list --filter 'name=^komodo-agent$' --quiet)' ]]; then
       docker compose -f $service_path/agents/compose.yaml --env-file $service_path/agents/.env --env-file $service_path/agents/.env.komodo-agent up komodo-agent -d > /dev/null 2>&1
 
       sleep 30
