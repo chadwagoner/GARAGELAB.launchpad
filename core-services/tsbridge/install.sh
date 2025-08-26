f3c5bd6c2f0c06d6665dc3d0f9216cbbc3df106a
Chad <chawagon03@gmail.com>
2025-08-25 21:19:20 -0400

    feat(core-services/tsbridge): add new service for tsbridge with docker compose and installation script

---
+++ core-services/tsbridge/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+### USER INPUT VARIABLES
+read -p '[REQUIRED] TAILSCALE OAUTH CLIENT ID: ' tailscale_oauth_id < /dev/tty
+if [[ -z $tailscale_oauth_id ]]; then
+  echo "ERROR: TAILSCALE OAUTH CLIENT ID REQUIRED... Exiting"
+  exit 1
+fi
+
+read -p '[REQUIRED] TAILSCALE OAUTH CLIENT SECRET: ' tailscale_oauth_secret < /dev/tty
+if [[ -z $tailscale_oauth_secret ]]; then
+  echo "ERROR: TAILSCALE OAUTH CLIENT SECRET REQUIRED... Exiting"
+  exit 1
+fi
+
+### MAKE CORE SERVICE DIRECTORIES
+mkdir -p /opt/core-services/tsbridge
+mkdir -p /opt/core-services/tsbridge/data
+
+### GET COMPOSE FILE
+curl -sL -o /opt/core-services/tsbridge/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core-services/tsbridge/compose.yaml
+
+### GET .ENV FILE
+curl -sL -o /opt/core-services/tsbridge/.env -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core-services/tsbridge/.env
+
+### REPLACE .ENV VARIABLES
+sed -i "s/__TS_OAUTH_CLIENT_ID__/$tailscale_oauth_id/g" /opt/core-services/tsbridge/.env
+sed -i "s/__TS_OAUTH_CLIENT_SECRET__/$tailscale_oauth_secret/g" /opt/core-services/tsbridge/.env
+
+### GET INIT FILE
+curl -sL -o /etc/init.d/container_tsbridge -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/alpine-linux/templates/init/container_tsbridge
+chmod 755 /etc/init.d/container_tsbridge
+
+### ENABLE BOOT START
+rc-update add contaienr_tsbridge default
+
+### START CORE SERVICE
+# rc-service container_tsbridge start
