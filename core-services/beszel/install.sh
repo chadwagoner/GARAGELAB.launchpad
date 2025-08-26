af735eb4fa59a0fc362c7d68686cf80982ceddd3
Chad <chawagon03@gmail.com>
2025-08-26 10:34:10 -0400

    feat(core-services/beszel): add script to prompt for and set the service URL in .env file

--- core-services/beszel/install.sh
+++ core-services/beszel/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 ### USER INPUT VARIABLES
+read -p '[REQUIRED] SERVICE URL [https://<service>.<tailnet>.ts.net]: ' service_url < /dev/tty
+if [[ -z $service_url ]]; then
+  echo "ERROR: SERVICE URL REQUIRED... Exiting"
+  exit 1
+fi
 
 ### MAKE BESZEL DIRECTORIES
 mkdir -p /opt/core-services/beszel
@@ -13,6 +18,7 @@ curl -sL -o /opt/core-services/beszel/compose.yaml -H 'Cache-Control: no-cache, 
 curl -sL -o /opt/core-services/beszel/.env -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core-services/beszel/.env
 
 ### REPLACE .ENV VARIABLES
+sed -i "s#__APP_URL__#$service_url#g" /opt/core-services/beszel/.env
 
 ### GET INIT FILE
 doas curl -sL -o /etc/init.d/beszel -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/alpine-linux/templates/init/beszel
