7e2f23568ecc963d60047f7816a9471fcb4bcf63
Chad <chawagon03@gmail.com>
2025-08-25 20:25:04 -0400

    fix(alpine-linux/local-build.sh): add step to switch to alpine user after reboot

--- alpine-linux/local-build.sh
+++ alpine-linux/local-build.sh
@@ -4,4 +4,4 @@ alpine launch --disk 20G --memory 4096 --name launchpad && \
 echo -e "PAUSING FOR 5 SECONDS" && \
 sleep 5 && \
 alpine exec launchpad 'setup-hostname launchpad && setup-user -a -f "" alpine && reboot' && \
-echo -e "WILL HAVE TO SET PASSWORD FOR ALPINE USER MANUALLY\n### alpine ssh launchpad > passwd alpine"
+echo -e "\n\nWILL HAVE TO SET PASSWORD FOR ALPINE USER MANUALLY\n### alpine ssh launchpad > passwd alpine\n### su - alpine"
