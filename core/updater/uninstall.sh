7b4def7c546a0f40bd5bfc6f44961905f6a27da1
Chad <chawagon03@gmail.com>
2025-09-07 22:37:55 -0400

    Symbol(clack:cancel)

--- core/updater/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### REMOVE CORE-UPDATER
-if [[ -e /etc/periodic/weekly/core-updater ]]; then
-  doas rm /etc/periodic/weekly/core-updater
-else
-  echo "ERROR: CORE-UPDATER DOES NOT EXIST... EXITING"
-  exit 1
-fi
