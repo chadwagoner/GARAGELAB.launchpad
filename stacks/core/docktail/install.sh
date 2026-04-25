1c715b6d910ae0c344c8d71a31eb4fa65f2bbbd3
Chad Wagoner <chawagon03@gmail.com>
2026-04-25 13:54:52 -0400

    feat(docktail install): update compose.yaml and .env source URLs to stacks

--- stacks/core/docktail/install.sh
+++ stacks/core/docktail/install.sh
@@ -17,10 +17,10 @@ fi
 mkdir -p /opt/core/docktail
 
 ### GET COMPOSE FILE
-curl -sL -o /opt/core/docktail/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/docktail/compose.yaml
+curl -sL -o /opt/core/docktail/compose.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/stacks/core/docktail/compose.yaml
 
 ### GET .ENV FILE
-curl -sL -o /opt/core/docktail/.env -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/core/docktail/.env
+curl -sL -o /opt/core/docktail/.env -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/GARAGELAB.launchpad/main/stacks/core/docktail/.env
 
 ### REPLACE .ENV VARIABLES
 sed -i "s/__TS_OAUTH_CLIENT_ID__/$tailscale_oauth_id/g" /opt/core/docktail/.env
